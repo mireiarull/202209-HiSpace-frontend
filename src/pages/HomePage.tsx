51835f632f5e4caf2cd4a58338230a6e678b0d91
Mireia Rull <mireia.rullmasdeu@gmail.com>
2022-11-09 13:51:57 +0100

    Add register component

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,7 @@
 import Button from "../components/Button/Button";
 import Header from "../components/Header/Header";
 import LoginForm from "../components/Login/Login";
+import RegisterForm from "../components/Register/Register";
 import RobotFilter from "../components/RobotFilter/RobotFilter";
 import RobotsList from "../components/RobotsList/RobotsList";
 import useUser from "../hooks/useUser";
@@ -13,6 +14,7 @@ const HomePage = () => {
   return (
     <>
       <Header />
+      <RegisterForm />
       {!loggedUser && <LoginForm />}
       {loggedUser && <h4>Welcome</h4>}
       {loggedUser && <Button text="Log out" action={logOut} />}
